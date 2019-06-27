32a7c07c52615111efee278c9d2035ceb3ba7067
kwh <kwh361@gmail.com>
2019-06-27 16:37:55 +0800

    commit 2

--- 狄克斯特拉算法.py
+++ 狄克斯特拉算法.py
@@ -1,39 +1,36 @@
 graph = {}
-graph["start"] = {}
-graph["start"]["a"] = 6
-graph["start"]["b"] = 2
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
 
-graph["a"] = {}
-graph["a"]["finish"] = 1
+graph['a'] = {}
+graph['a']['finish'] = 1
 
-graph["b"] = {}
-graph["b"]["a"] = 3
-graph["b"]["finish"] = 1
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['finish'] = 5
 
-graph["finish"] = {}
+graph['finish'] = {}
 
-infinity = float("inf")
+infinity = float('inf')
 costs = {}
-costs["a"] = 6
-costs["b"] = 2
-costs["finish"] = infinity
+costs['a'] = 6
+costs['b'] = 2
+costs['finish'] = infinity
 
 parents = {}
-
-parents["a"] = "start"
-parents["b"] = "start"
-parents["finish"] = "None"
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['finish'] = 'None'
 
 processed = []
 print(costs)
-print(graph)
-
 def find_lowest_cost_node(costs):
-    low_costs = float("inf")
+    low_costs = float('inf')
     low_costs_node = None
     for node in costs:
         cost = costs[node]
-        if cost<low_costs and node not in processed:
+        if cost < low_costs and node not in processed:
             low_costs = cost
             low_costs_node = node
     return low_costs_node
@@ -51,4 +48,5 @@ while node is not None:
     processed.append(node)
     node = find_lowest_cost_node(costs)
 
-print(costs)
+if __name__ == '__main__' :
+    print(costs)
